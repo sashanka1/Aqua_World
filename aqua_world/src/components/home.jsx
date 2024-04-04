2655361fa8685bf8acf49acbdb2b3d23c736b79e
Sashanka sekhar samal <sashankasekharsamal777@gmail.com>
2024-04-04 09:31:08 +0530

    some modification done

--- aqua_world/src/components/home.jsx
+++ aqua_world/src/components/home.jsx
@@ -46,16 +46,18 @@ useEffect(()=>{
 
        <>
        <div className="home">
-        <div className="fristimgs">
+        {/* <div className="fristimgs">
             <img src={displayImage}  alt ="img"/>
-        </div>
+        </div> */}
 <div className="catagory">
     <div onClick={()=>{handlecatagory("fish")}}> <img src="https://thumbs.dreamstime.com/b/goldfish-3621584.jpg" alt="fish" /></div>
     <div onClick={()=>{handlecatagory("plant")}}> <img src="https://media.istockphoto.com/photos/water-plants-picture-id176956989?k=20&m=176956989&s=612x612&w=0&h=269vYjYpxISojmxU6QaONY04qjUbKma1VF2DSnDtlh8=" alt="plant" /></div>
-    <div onClick={()=>{handlecatagory("aquarium")}}> <img src="https://i.ebayimg.com/images/g/Vh0AAOSwYBRgriQe/s-l500.png" alt="aquarium" /></div>
+    <div onClick={()=>{handlecatagory("fish")}}> <img src="https://4.imimg.com/data4/AI/KC/MY-30251834/aquarium-fish-500x500.jpg" alt="fish" /></div>
+    {/* <div onClick={()=>{handlecatagory("aquarium")}}> <img src="https://i.ebayimg.com/images/g/Vh0AAOSwYBRgriQe/s-l500.png" alt="aquarium" /></div> */}
     <div onClick={()=>{handlecatagory("accessories")}}> <img src="https://rukminim1.flixcart.com/image/612/612/kwxv98w0/aquarium-substrate/7/v/c/1-psp-sky-original-imag9gf4sfquzyjf.jpeg?q=70" alt="accessories" /></div>
-    <div> <img onClick={()=>{handlecatagory("aquarium")}} src="https://i.ebayimg.com/images/g/Vh0AAOSwYBRgriQe/s-l500.png" alt="aquarium" /></div>
-    <div> <img onClick={()=>{handlecatagory("accessories")}} src="https://rukminim1.flixcart.com/image/612/612/kwxv98w0/aquarium-substrate/7/v/c/1-psp-sky-original-imag9gf4sfquzyjf.jpeg?q=70" alt="accessories" /></div>
+    {/* <div> <img onClick={()=>{handlecatagory("aquarium")}} src="https://i.ebayimg.com/images/g/Vh0AAOSwYBRgriQe/s-l500.png" alt="aquarium" /></div> */}
+    <div onClick={()=>{handlecatagory("plant")}}> <img src="https://tropica.com/media/2562031/det-rigtige-akvarium-forside-1000px.jpg" alt="plant" /></div>
+    <div> <img onClick={()=>{handlecatagory("accessories")}} src="https://m.media-amazon.com/images/I/81UNNm5x8nL._AC_UF894,1000_QL80_.jpg" alt="accessories" /></div>
     
 
 
